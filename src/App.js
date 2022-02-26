7f8e529837573f27d66f49b8da28bc9ff860bdc7
Edina Kanlić <100413941+edinakanlic@users.noreply.github.com>
2022-02-26 14:43:39 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Sidebar from './components/Sidebar';
+import Submenu from './components/Submenu';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Sidebar />
+      <Hero />
+      <Submenu />
+    </>
+  );
+}
+
+export default App;
